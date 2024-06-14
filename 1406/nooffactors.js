d5097ca2667292b8bcaa9157db483d34c83e1e3a
pragyanmayeem <pragyanmayeem@main.com>
2024-06-15 00:05:29 +0530

    loop in js

---
+++ 1406/nooffactors.js
@@ -0,0 +1,9 @@
+let n=6;
+let c=0;
+console.log("number is",n);
+for(let i=1;i<=n;i++){
+    if(n%i==0){
+        c=c+1;
+    }
+}
+console.log("number of factors are",c);
---
+++ 1406/reverseorder.js
@@ -0,0 +1,8 @@
+let n=6;
+console.log("number is",n);
+console.log("factors are");
+for(let i=n;i>=1;i--){
+    if(n%i==0){
+        console.log(i);
+    }
+}
